10964a9f0ff4878c925423f65dd7a2db9c0f388a
Bluice Zhen <bluice.zhen@gmail.com>
2018-05-30 17:27:15 +0800

    add args rabbitmq username and password

--- main.py
+++ main.py
@@ -10,9 +10,11 @@ from write_log import write_log
 @click.option("--rmq-host", default="localhost", help="Host bind of RabbitMQ.")
 @click.option("--rmq-port", default="5672", help="Port bind of RabbitMQ.")
 @click.option("--rmq-queue", default="logs", help="Queue name of RabbitMQ.")
+@click.option("--rmq-username", default="guest", help="Username of RabbitMQ.")
+@click.option("--rmq-password", default="guest", help="Password name of RabbitMQ.")
 @click.option("--logs-dir", default="./tmp", help="Director for storage logs file.")
 @click.option("--debug", default="false")
-def main(rmq_host, rmq_port, rmq_queue, logs_dir, debug):
+def main(rmq_host, rmq_port, rmq_queue, rmq_username, rmq_password, logs_dir, debug):
     try:
         port = int(rmq_port)
 
@@ -21,12 +23,12 @@ def main(rmq_host, rmq_port, rmq_queue, logs_dir, debug):
         else:
             app_logger = get_app_logger(logging.INFO, stream_out=False, file_path=f"{ logs_dir }/logs.log")
 
-        connection = pika.BlockingConnection(pika.ConnectionParameters(host=rmq_host, port=port))
+        connect_url = f"amqp://{ rmq_username }:{ rmq_password }@{ rmq_host }:{ port }"
+        connection = pika.BlockingConnection(pika.URLParameters(connect_url))
         channel = connection.channel()
         channel.queue_declare(queue=rmq_queue)
         channel.basic_consume(functools.partial(write_log, app_logger, logs_dir),
-                              queue=rmq_queue,
-                              arguments={"aaa": 111})
+                              queue=rmq_queue)
         channel.start_consuming()
     except ValueError:
         print("Illegal Port")
--- test_producer.py
+++ test_producer.py
@@ -4,7 +4,7 @@ import json
 if __name__ == "__main__":
     # APP_NAME, LOG_LEVEL, ACTION, CONTENT
     body = ["TEST", "INFO", "Login", "Password error1121212"]
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+    connection = pika.BlockingConnection(pika.URLParameters("amqp://guest:guest@localhost:5672"))
     channel = connection.channel()
     channel.queue_declare(queue='logs')
     channel.basic_publish(exchange='', routing_key='logs', body=json.dumps(body, ensure_ascii=False))
